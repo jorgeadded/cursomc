378b56e631023aef6c466cfcad56f976d8fe3282
Jorge Added <jorgeadded@gmail.com>
2020-11-18 22:20:10 -0300

    testando a categoria

--- src/main/java/com/jorge/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/jorge/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,26 @@
 package com.jorge.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jorge.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String listar() {
-		return "rest está funcionando!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
